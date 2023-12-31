ef1b03222d3b9f237076ab110f4292bd54dd2aab
Varun Maheshwari <maheshwarivarun82@gmail.com>
2023-12-31 13:51:17 +0530

    "Completed city repositort"

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,26 +1,59 @@
 const {City}=require('../models/index')
 
 class CityRepository {
-    async createCity({ name }){
+    
+
+    async createCity({name})  { //{name:"new delhi"}
         try{
-            const city=await City.create({ name });
+            const city=await City.create({
+                name
+            })
             return city;
-        }catch(error){
+        }catch (error){
+            console.log("Something went wrong in the repositary layer")
             throw {error};
         }
     }
 
-    async deleteCity(cityID){
-        try{
+    async deleteCity(cityId)  {
+        try {
             await City.destroy({
                 where:{
-                    id:cityID
+                    id: cityId
                 }
             });
-        }catch(error){
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in the repositary layer")
             throw {error};
         }
     }
+
+    async updateCity(cityId,data)  {{
+        try {
+            const city=await City.update(data,{
+                where:{
+                    id: cityId
+                }
+            })
+            return city
+        } catch (error) {
+            console.log("Something went wrong in the repositary layer")
+            throw {error};
+        }
+    }}
+
+    async getCity(cityId) {
+        try {
+            const city=await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repositary layer")
+            throw {error};
+        }
+    }
+
+
 }
 
 module.exports=CityRepository
