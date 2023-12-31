7f87fd3655f20e6f815e88f97d3bbbeed1bf9993
Varun Maheshwari <maheshwarivarun82@gmail.com>
2024-01-01 01:55:49 +0530

    added flights search crud with few filters

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -16,13 +16,33 @@ const create=async(req,res)=>{
         return res.status(500).json({
             data:{},
             success:false,
-            message:'Not able to fetch the flight',
+            message:'Not able to create the flight',
             err:error
         });
     }
 }
 
+const getAll=async (req,res)=>{
+    try {
+        const response=await flightService.getAllFlightData(req.query);
+        return res.status(200).json({
+            data: response,
+            success:true,
+            err:{},
+            message: 'Successfully fetched the flights'
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message:'Not able to fetch the flights',
+            err:error
+        });
+    }
+}
 
 module.exports={
-    create
+    create,
+    getAll
 }
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -25,8 +25,14 @@ class FlightService{
         }
     }
 
-    async getFlightData(){
-        // todo 
+    async getAllFlightData(data){
+        try {
+            const flights=await this.flightRepository.getAllFlights(data);
+            return flights;
+        } catch (error) {
+            console.log("Some thing went wrong in the repository layer")
+            throw {error};
+        }
     }
 }
 
